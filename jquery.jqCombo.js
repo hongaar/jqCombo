7b5b501d3fdcae37e4269a3cc1cf74ec8c00a5f1
Joram van den Boezem <hongaar@gmail.com>
2012-07-20 16:20:01 +0200

    fixed bug where shift-tab on INPUT in demo page would leave the keypressedCounter > 0

--- jquery.jqCombo.js
+++ jquery.jqCombo.js
@@ -59,6 +59,7 @@
 	};
 	
 	var keys = {
+		tab: 9,
 		ignore: [			
 			35, // end
 			36, // home
@@ -123,7 +124,7 @@
 				_watchChanges($select, $input);
 				
 				// Keypress counter and repeater neutralizer
-				_initKeypressCounter($input);
+				_initKeypressCounter();
 				
 				// Autocompletes the input when typing
 				_autocompleteInput($select, $input, _options.notfoundCss);
@@ -269,70 +270,76 @@
 		var origInputCss = _origCss($input, notfoundCss);
 		
 		$input.on('keyup.jqcombo', function(e) {
-			// Sore last pressed key for lookback
-			if ($.inArray(e.keyCode, keys.lookback) == -1) {
-				lastKeycode = e.keyCode;
-			}
-			
-			// Select all on tab
-			if (e.keyCode === 9 ||
-				($.inArray(e.keyCode, keys.lookback) >= 0 && lastKeycode === 9)) {
-				$input.select();
-				return;
-			}
-			
-			// Arrow navigation
-			if ($.inArray(e.keyCode, keys.down) >= 0) {
-				var o = $select.find('option:selected').next();	
-				$select.val(o.val());
-				$input.val(o.text()).select();
-				return;
-			} else if ($.inArray(e.keyCode, keys.up) >= 0) {
-				var o = $select.find('option:selected').prev();	
-				$select.val(o.val());
-				$input.val(o.text()).select();
-				return;
-			}
-			
-			// Wait for all keys to be released
-			if (keypressCounter > 0) {
-				return;
-			}
-			
-			// Ignore the current or lookback key?
-			if ($.inArray(e.keyCode, keys.ignore) >= 0 ||
-				($.inArray(e.keyCode, keys.lookback) >= 0 && $.inArray(lastKeycode, keys.ignore) >= 0)) {
-				return;
-			}
-			
-			// Resets the notfound color
-			$input.css(origInputCss);
-			
-			// Gets the current input text
-			var typedText = $input.val();
-			
-			// Find an option containing our text (case-insensitive)
-			var $match = $select.find('option:startswithi("' + typedText + '"):eq(0)');
-			var matchedText = $match.text();
-			
-			// Do we have a match?
-			if ($match.length) {
-				// Set select box to match
-				$select.val($match.val());
-				
-				// Make selection if not in noselectionKeys list
-				if ($.inArray(e.keyCode, keys.noselection) == -1) {
-					$input.val(matchedText);
-					_createSelection($input, typedText.length, matchedText.length)
-				}				
-			} else {
-				// Set select box to option without value
-				// TODO: if no such option exist?
-				$select.val('');
-				
-				// Set notfound color on input
-				$input.css(notfoundCss);
-			}			
+			// Make entire code of keyup event asynchronous to allow for
+			// event to bubble up to document first to get a decent indication
+			// of state of pressed keys
+			setTimeout(function() {
+
+				// Sore last pressed key for lookback
+				if ($.inArray(e.keyCode, keys.lookback) == -1) {
+					lastKeycode = e.keyCode;
+				}
+
+				// Select all on tab
+				if (e.keyCode === keys.tab ||
+					($.inArray(e.keyCode, keys.lookback) >= 0 && lastKeycode === keys.tab)) {
+					$input.select();
+					return;
+				}
+
+				// Arrow navigation
+				if ($.inArray(e.keyCode, keys.down) >= 0) {
+					var o = $select.find('option:selected').next();	
+					$select.val(o.val());
+					$input.val(o.text()).select();
+					return;
+				} else if ($.inArray(e.keyCode, keys.up) >= 0) {
+					var o = $select.find('option:selected').prev();	
+					$select.val(o.val());
+					$input.val(o.text()).select();
+					return;
+				}
+
+				// Wait for all keys to be released
+				if (keypressCounter > 0) {
+					return;
+				}
+
+				// Ignore the current or lookback key?
+				if ($.inArray(e.keyCode, keys.ignore) >= 0 ||
+					($.inArray(e.keyCode, keys.lookback) >= 0 && $.inArray(lastKeycode, keys.ignore) >= 0)) {
+					return;
+				}
+
+				// Resets the notfound color
+				$input.css(origInputCss);
+
+				// Gets the current input text
+				var typedText = $input.val();
+
+				// Find an option containing our text (case-insensitive)
+				var $match = $select.find('option:startswithi("' + typedText + '"):eq(0)');
+				var matchedText = $match.text();
+
+				// Do we have a match?
+				if ($match.length) {
+					// Set select box to match
+					$select.val($match.val());
+
+					// Make selection if not in noselectionKeys list
+					if ($.inArray(e.keyCode, keys.noselection) == -1) {
+						$input.val(matchedText);
+						_createSelection($input, typedText.length, matchedText.length)
+					}				
+				} else {
+					// Set select box to option without value
+					// TODO: if no such option exist?
+					$select.val('');
+
+					// Set notfound color on input
+					$input.css(notfoundCss);
+				}
+			}, 0);
 		});
 	}
 	
@@ -411,9 +418,10 @@
 	// Keypress counter and repeater neutralizer
 	// Used in autocompleter to only continue if 
 	// user finished pressing any keys
-	function _initKeypressCounter($input) {
+	function _initKeypressCounter() {
 		var keysPressed = [];
-		$input.on('keydown.jqcombo', function(e) {
+		var $target = $(document);
+		$target.bind('keydown.jqcombo', function(e) {
 			// Store currently pressed keys
 			if ($.inArray(e.keyCode, keysPressed) == -1) {
 				keysPressed.push(e.keyCode);
@@ -421,7 +429,7 @@
 			keypressCounter = keysPressed.length;
 		});
 		
-		$input.on('keyup.jqcombo', function(e) {			
+		$target.bind('keyup.jqcombo', function(e) {			
 			// Remove currently pressed key from store
 			var index = $.inArray(e.keyCode, keysPressed);
 			if (index >= 0) {
